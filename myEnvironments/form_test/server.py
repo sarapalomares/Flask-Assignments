85d9598bce19d2452f35efd86b908f8df269671f
Sara Palomares <sarapalomares@me.com>
2017-01-17 09:39:17 -0800

    New Flask Environments

--- myEnvironments/form_test/server.py
+++ myEnvironments/form_test/server.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session
 app = Flask(__name__)
-# our index route will handle rendering our form
+app.secret_key = 'ThisIsSecret'
+
 @app.route('/')
 def index():
   return render_template("index.html")
@@ -11,8 +12,12 @@ def create_user():
    print "Got Post Info"
    # we'll talk about the following two lines after we learn a little more
    # about forms
-   name = request.form['name']
-   email = request.form['email']
-   # redirects back to the '/' route
-   return redirect('/')
+   session['name'] = request.form['name']
+   session['email'] = request.form['email']
+   return redirect('/show')
+
+@app.route('/show')
+def show_user():
+    return render_template('user.html')
+
 app.run(debug=True) # run our server
